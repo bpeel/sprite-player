15cb9aa9138ab5c6cc549278d7d6175a6c7c3444
Neil Roberts <neil@linux.intel.com>
2013-08-24 15:48:08 +0100

    Use GOptionContext to parse the arguments
    The URI argument is now parsed with glib so that it will be easier to
    add more options later. The URI is now checked whether it is actually
    a path to an existing file and if so it will automatically generate a
    file:/// URI for it.

--- src/sprite-player.c
+++ src/sprite-player.c
@@ -27,6 +27,7 @@
 
 #include <cogl/cogl.h>
 #include <cogl-gst/cogl-gst.h>
+#include <gio/gio.h>
 
 #include "effects.h"
 
@@ -45,6 +46,55 @@ typedef struct _Data
   const Effect *current_effect;
   void *effect_data;
 } Data;
+
+typedef enum
+{
+  VIDEO_TYPE_NONE,
+  VIDEO_TYPE_FILE
+} VideoType;
+
+static VideoType opt_video_type = VIDEO_TYPE_NONE;
+static const char *opt_video_file = NULL;
+
+static gboolean
+set_video_type (VideoType type,
+                GError **error)
+{
+  if (opt_video_type != VIDEO_TYPE_NONE)
+    {
+      g_set_error (error,
+                   G_OPTION_ERROR,
+                   G_OPTION_ERROR_BAD_VALUE,
+                   "Only one video source can be specified");
+      return FALSE;
+    }
+
+  opt_video_type = type;
+
+  return TRUE;
+}
+
+static gboolean
+opt_filename_cb (const char *option_name,
+                 const char *value,
+                 void *data,
+                 GError **error)
+{
+  if (!set_video_type (VIDEO_TYPE_FILE, error))
+    return FALSE;
+
+  opt_video_file = g_strdup (value);
+
+  return TRUE;
+}
+
+static GOptionEntry
+main_options[] =
+  {
+    { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_CALLBACK, &opt_filename_cb,
+      "File or URL to play", "FILE" },
+    { NULL, 0, 0, 0, NULL, NULL, NULL }
+  };
 
 static gboolean
 bus_watch (GstBus *bus,
@@ -203,6 +253,41 @@ set_effect (Data *data,
     effect->set_up_pipeline (data->sink, data->effect_data);
 }
 
+static gboolean
+process_arguments (int *argc,
+                   char ***argv,
+                   GError **error)
+{
+  GOptionContext *context;
+  gboolean ret;
+  GOptionGroup *group, *gst_group;
+
+  group = g_option_group_new (NULL, /* name */
+                              NULL, /* description */
+                              NULL, /* help_description */
+                              NULL, /* user_data */
+                              NULL /* destroy notify */);
+  g_option_group_add_entries (group, main_options);
+  context = g_option_context_new ("- A sample video player with effects");
+  g_option_context_set_main_group (context, group);
+
+  gst_group = gst_init_get_option_group ();
+  g_option_context_add_group (context, gst_group);
+
+  ret = g_option_context_parse (context, argc, argv, error);
+
+  g_option_context_free (context);
+
+  if (ret && *argc > 1)
+    {
+      g_set_error (error, G_OPTION_ERROR, G_OPTION_ERROR_UNKNOWN_OPTION,
+                   "Unknown option '%s'", (* argv)[1]);
+      ret = FALSE;
+    }
+
+  return ret;
+}
+
 int
 main (int argc,
       char **argv)
@@ -213,8 +298,15 @@ main (int argc,
   GstElement *pipeline;
   GstElement *bin;
   GSource *cogl_source;
+  GError *error = NULL;
   GstBus *bus;
-  char *uri;
+
+  if (!process_arguments (&argc, &argv, &error))
+    {
+      fprintf (stderr, "%s\n", error->message);
+      g_clear_error (&error);
+      return 1;
+    }
 
   memset (&data, 0, sizeof (Data));
 
@@ -229,25 +321,34 @@ main (int argc,
 
   data.fb = COGL_FRAMEBUFFER (onscreen);
 
-  /* Intialize GStreamer */
-
-  gst_init (&argc, &argv);
-
   data.sink = cogl_gst_video_sink_new (ctx);
 
   pipeline = gst_pipeline_new ("gst-player");
   bin = gst_element_factory_make ("playbin", "bin");
 
-  if (argc < 2)
-    uri = "http://docs.gstreamer.com/media/sintel_trailer-480p.webm";
-  else
-    uri = argv[1];
+  if (opt_video_type == VIDEO_TYPE_NONE)
+    {
+      opt_video_file =
+        "http://docs.gstreamer.com/media/sintel_trailer-480p.webm";
+      opt_video_type = VIDEO_TYPE_FILE;
+    }
 
   g_object_set (G_OBJECT (bin), "video-sink", GST_ELEMENT (data.sink), NULL);
 
   gst_bin_add (GST_BIN (pipeline), bin);
 
-  g_object_set (G_OBJECT (bin), "uri", uri, NULL);
+  if (g_file_test (opt_video_file, G_FILE_TEST_EXISTS))
+    {
+      GFile *file = g_file_new_for_path (opt_video_file);
+      char *uri = g_file_get_uri (file);
+
+      g_object_set (G_OBJECT (bin), "uri", uri, NULL);
+
+      g_free (uri);
+      g_object_unref (file);
+    }
+  else
+    g_object_set (G_OBJECT (bin), "uri", opt_video_file, NULL);
 
   set_effect (&data, effects[0]);
 
